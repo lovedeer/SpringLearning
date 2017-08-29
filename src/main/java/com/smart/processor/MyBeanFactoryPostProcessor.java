e6bee3d5d6f84359934eb1d30df5ba8eb1df0285
zhoulong <zhoulong@koal.com>
2017-08-29 23:28:47 +0800

    update

---
+++ src/main/java/com/smart/processor/MyBeanFactoryPostProcessor.java
@@ -0,0 +1,15 @@
+package com.smart.processor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        for (String str : beanFactory.getBeanDefinitionNames()) {
+            System.out.println(str);
+        }
+
+    }
+}
