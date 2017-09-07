77c81f3826a4820b708d805f4eafca09f36d77fe
zhoulong <zhoulong@koal.com>
2017-09-07 23:36:39 +0800

    update

--- src/main/java/com/smart/domain/Board.java
+++ src/main/java/com/smart/domain/Board.java
@@ -25,7 +25,7 @@ public class Board extends BaseDomain {
     @Column(name = "topic_num")
     private int topicNum;
 
-    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, mappedBy = "manBoards", fetch = FetchType.LAZY)
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, mappedBy = "manBoards", fetch = FetchType.EAGER)
     private Set<User> users = new HashSet<User>();
 
     public int getTopicNum() {
