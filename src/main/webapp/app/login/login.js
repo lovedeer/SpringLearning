3294d712dcbed51785b7d6d4fe755daecf01bfd7
zhoulong <zhoulong@koal.com>
2017-10-11 22:39:36 +0800

    update

--- src/main/webapp/app/login/login.js
+++ src/main/webapp/app/login/login.js
@@ -1,11 +1,20 @@
-angular.module("loginApp", []).controller("loginCtrl",  function () {
+angular.module("loginApp", ['ngResource']).controller("loginCtrl", ['$resource', '$location', function ($resource, $location) {
     this.reset = function () {
         this.username = "";
         this.password = "";
     };
     this.error = false;
+    var self = this;
+    var User = $resource('login/username/:username/password/:password');
 
     this.login = function () {
-
+        var user = User.get({username: this.username, password: this.password}, function () {
+            if (user.userName == null) {
+                self.error = true;
+            }
+            else {
+               window.location="index.html";
+            }
+        });
     };
-});
+}]);
